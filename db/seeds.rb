8e68b88ba30900ba8886bdc4d1488d5a0eb5b8d3
teppcoff <mt2000115@icloud.com>
2021-11-16 16:27:11 +0900

    add top layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 table_names = %w(
-  schools subjects staff_members parent_members student_members semesters periods goal_sheets 
+  schools subjects staff_members parent_members student_members semesters periods
   textbooks daily_sheets notifications events examinations score_sheets scores
 )
 
