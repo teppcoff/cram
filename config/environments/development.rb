12d4dfd2597b7f9afccdf60c497b17ebc469c26a
teppcoff <mt2000115@icloud.com>
2021-11-19 06:06:15 +0900

    improve start.sh

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,6 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
 
-  config.hosts << "cramapp.herokuapp.com" #追記
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
