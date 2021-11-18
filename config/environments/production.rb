0d2a7844dccae00f54c87a606cb658717c57bf06
teppcoff <mt2000115@icloud.com>
2021-11-19 05:57:30 +0900

    improve environment development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
 
-  config.hosts << "cramapp.herokuapp.com" #追記
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
