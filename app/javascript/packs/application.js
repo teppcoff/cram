4896de1404abcabc468a529d543e49ba1d3fb2ac
teppcoff <mt2000115@icloud.com>
2021-11-24 00:15:55 +0900

    setting fullcalendar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@fullcalendar/common/main.css'
+import '@fullcalendar/daygrid/main.css'
 require('jquery')
 require('@nathanvda/cocoon')
 
