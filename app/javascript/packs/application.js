fc7eb9808a88c72f3103e07da9d799474b2104d8
teppcoff <mt2000115@icloud.com>
2021-11-24 21:00:08 +0900

    change webpakcker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,6 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 require('jquery')
 require('@nathanvda/cocoon')
-require("../calendar/staff_member_event.js")
-require("../calendar/student_member_event.js")
-require("../calendar/parent_member_event.js")
 
 Rails.start()
 Turbolinks.start()
