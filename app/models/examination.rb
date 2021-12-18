6e996030028fe8030f741706df63e5848938a8cf
teppcoff <mt2000115@icloud.com>
2021-12-18 12:14:35 +0900

    improve examination model

--- app/models/examination.rb
+++ app/models/examination.rb
@@ -5,73 +5,57 @@ class Examination < ApplicationRecord
     belongs_to :school
     has_many :score_sheets
 
-    def calculate_total_average
-        score_sheets = self.score_sheets
-        total_points = 0
-        score_sheets.each do |sheet|
-            total_points += sheet.total_point
-        end
-        ( total_points / score_sheets.count ).round(1)
+    def perfect_score
+        subjects_of_examination.count * 100
+    end
+
+    def number_of_takers
+        score_sheets.count
+    end
+
+    def calculate_average_total_point
+        total_point = 0
+        score_sheets.each { |sheet| total_point += sheet.total_point }
+        ( total_point / score_sheets.count ).round(1)
     end
 
     def calculate_highest_total_point
-        score_sheets = self.score_sheets
         highest_total_point = 0
-        score_sheets.each do |sheet|
-            highest_total_point = sheet.total_point if sheet.total_point > highest_total_point
-        end
+        score_sheets.each { |sheet| highest_total_point = sheet.total_point if sheet.total_point > highest_total_point }
         highest_total_point
     end
 
     def calculate_lowest_total_point
-        score_sheets = self.score_sheets
-        lowest_total_point = 500
-        score_sheets.each do |sheet|
-            lowest_total_point = sheet.total_point if sheet.total_point < lowest_total_point
-        end
+        lowest_total_point = 1000
+        score_sheets.each { |sheet| lowest_total_point = sheet.total_point if sheet.total_point < lowest_total_point }
         lowest_total_point
     end
 
-    def calculate_average_of(subject)
-        score_sheets = self.score_sheets
+    def scores_list_of(subject)
         scores_list = Array.new
-        score_sheets.each do |sheet| 
+        score_sheets.each do |sheet|
             sheet.scores.each do |score|
                 scores_list.push(score.point) if score.subject_id == subject
             end
         end
-        ( scores_list.sum / (scores_list.length) ).round(1)
+        scores_list
+    end
+
+    def calculate_average_of(subject)
+        ( scores_list_of(subject).sum / (scores_list_of(subject).length) ).round(1)
     end
 
     def calculate_highest_point_of(subject)
-        score_sheets = self.score_sheets
-        highest_point = 0
-        score_sheets.each do |sheet|
-            sheet.scores.each do |score|
-                if score.subject_id == subject && score.point > highest_point
-                    highest_point = score.point
-                end
-            end
-        end
-        highest_point
+        scores_list_of(subject).max
     end
 
     def calculate_lowest_point_of(subject)
-        score_sheets = self.score_sheets
-        lowest_point = 100
-        score_sheets.each do |sheet|
-            sheet.scores.each do |score|
-                if score.subject_id == subject && score.point < lowest_point
-                    lowest_point = score.point
-                end
-            end
-        end
-        lowest_point
+        scores_list_of(subject).min
     end
 
     def subjects_of_examination
         subjects_list = Array.new
-        self.score_sheets.each do |sheet|
+        score_sheets.each do |sheet|
             sheet.scores.each { |score| subjects_list << score.subject }
         end 
         subjects_list.uniq
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,7 +3,7 @@ class Subject < ApplicationRecord
     validates :name, presence: true
 
     has_many :takes, dependent: :destroy
-    has_many :student_members, :through => :takes
+    has_many :student_members, through: :takes
     has_many :textbooks
     has_many :events
     has_many :daily_sheets
