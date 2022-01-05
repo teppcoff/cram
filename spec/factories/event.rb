6c9afea91c6345b41d166bd2dabda361da3b6ec9
teppcoff <mt2000115@icloud.com>
2022-01-06 01:04:24 +0900

    improve test

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -5,10 +5,7 @@ FactoryBot.define do
         association :subject
         association :period
         sequence(:title) { |n| "授業#{n}" }
-        repeats_on { 1 }
         starts_at { "17:00:00" }
         ends_at { "18:00:00" }
-        starts_on { "2021-11-01" }
-        ends_on { "2021-12-05" }
     end
 end
